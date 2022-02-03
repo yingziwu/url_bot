638c2083bc5427cd332fc976fd43a9e01a593772
bgme <i@bgme.me>
2022-02-03 16:29:08 +0800

    增加 delete 指令提示文本

--- src/index.ts
+++ src/index.ts
@@ -213,7 +213,7 @@ function openStream(id: string, acct: string) {
         }
         const text = "发现含有追踪参数的链接或短链接，详情如下：\n\n" +
           texts.join("\n\n") +
-          "\n\n" +
+          "\n\n" + "当您删除含有追踪参数的链接或短链接的嘟文后，可使用 delete 指令删除本条回复嘟文。\n更多信息可参见：https://bgme.me/@url/107733097656542346" + '\n\n'
           _mentions.map((m) => `@${m}`).join(" ");
         return text;
       }
